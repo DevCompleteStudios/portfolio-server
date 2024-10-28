fe44c449ce63de9c33ab572dc397ec16d1e7ebfd
DevCompleteStudios <jorge.yael333@gmail.com>
2024-10-28 07:20:48 -0600

    Retornar la respuesta en con el error, fecha y status

--- src/main/java/com/devstudios/portfolio/portfolio_backend/presentation/interceptors/HandleErrors.java
+++ src/main/java/com/devstudios/portfolio/portfolio_backend/presentation/interceptors/HandleErrors.java
@@ -1,6 +1,9 @@
 package com.devstudios.portfolio.portfolio_backend.presentation.interceptors;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
@@ -15,17 +18,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class HandleErrors {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<String> handleDuplicatedKeys( DataIntegrityViolationException ex ){
+    public ResponseEntity<Map<String, Object>> handleDuplicatedKeys( DataIntegrityViolationException ex ){
         String key = ex.getMessage().split("\\(")[1].split("\\)")[0];
         String value = ex.getMessage().split("\\(")[2].split("\\)")[0];
 
         String message = "Coment with '" + key + ": " + value + "' already exists";
 
-        return ResponseEntity.status(200).body(message);
+        return getResponse(message, 400);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List<String>> handleBadRequest( MethodArgumentNotValidException ex ){
+    public ResponseEntity<Map<String, Object>> handleBadRequest( MethodArgumentNotValidException ex ){
         List<String> errors = ex.getBindingResult()
             .getFieldErrors().stream()
                 .map( e -> {
@@ -35,7 +38,17 @@ public class HandleErrors {
                     return field + ": " + err;
                 }).toList();
 
-        return ResponseEntity.status(400).body(errors);
+        return getResponse(errors, 400);
+    }
+
+    private ResponseEntity<Map<String, Object>> getResponse( Object err, int status ){
+        Map<String, Object> res = new HashMap<>();
+
+        res.put("err", err);
+        res.put("date", LocalDate.now());
+        res.put("status", status);
+
+        return ResponseEntity.status(status).body(res);
     }
 
 }
