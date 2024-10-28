020b8a28b8fc9e0651cfc380cf7450772f456ac4
DevCompleteStudios <jorge.yael333@gmail.com>
2024-10-27 21:09:58 -0600

    Agregando entidad de comentario

---
+++ src/main/java/com/devstudios/portfolio/portfolio_backend/domain/entities/ComentEntity.java
@@ -0,0 +1,63 @@
+package com.devstudios.portfolio.portfolio_backend.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+
+@Entity
+@Table(name = "coments")
+public class ComentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique=true)
+    private String email;
+
+    private String coment;
+
+    @Column(unique=true)
+    private String username;
+    private int stars;
+
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getComent() {
+        return coment;
+    }
+    public void setComent(String coment) {
+        this.coment = coment;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public int getStars() {
+        return stars;
+    }
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+}
